52a13bf55b17cc4f541ea32a917599644be9ac27
zegaines <zegaines@cpp.edu>
2024-10-22 15:11:55 -0700

    inference over cifar100

--- resnet/resnet_cifar100.py
+++ resnet/resnet_cifar100.py
@@ -3,14 +3,16 @@ from transformers import AutoImageProcessor, ResNetForImageClassification
 from datasets import load_dataset
 from PIL import Image
 import os
+import numpy as np
 
 def load_model_and_processor(model_name="microsoft/resnet-152"):
     image_processor = AutoImageProcessor.from_pretrained(model_name)
     model = ResNetForImageClassification.from_pretrained(model_name)
     return model, image_processor
 
-def process_image(image_path, image_processor):
-    image = Image.open(image_path).convert("RGB")
+def process_image(image, image_processor):
+    if isinstance(image,np.ndarray):
+         image = Image.fromarray(image)
     inputs = image_processor(images=image, return_tensors="pt")
     return inputs
 
@@ -22,20 +24,23 @@ def classify_image(model, inputs):
     return predicted_class_idx
 
 def main(model_name="microsoft/resnet-152"):
-    dataset = load_dataset("imagenet-1k",split="val",cache_dir="/media/scalesagx/extra")
+    dataset = load_dataset("uoft-cs/cifar100",split="test")
     model, image_processor = load_model_and_processor(model_name)
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     model = model.to(device)
 
-    for example in dataset:
-            image = example["image"]
-            inputs = process_image(image, image_processor)
+    for i, example in enumerate(dataset):
+        
+            inputs = process_image(example["img"], image_processor)
             inputs = {k: v.to(device) for k, v in inputs.items()}
             
             predicted_class_idx = classify_image(model, inputs)
             predicted_class = model.config.id2label[predicted_class_idx]
             
-            print(f"Image ID: {example['image_id']}, Predicted class: {predicted_class}")
+            print(f"Image {i}, Predicted class: {predicted_class}")
+
+            #if i >=10:
+            #    break
 
 if __name__ == "__main__":
     main()
